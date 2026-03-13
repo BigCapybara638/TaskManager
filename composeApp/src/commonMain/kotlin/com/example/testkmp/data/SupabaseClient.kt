ab0ab2822bd8cb902632f4ba9d692402500a427a
firekat <sp2007pavlov@yandex.ru>
2026-03-13 21:59:05 +0300

    add supabase client from deploy ios

---
+++ composeApp/src/commonMain/kotlin/com/example/testkmp/data/SupabaseClient.kt
@@ -0,0 +1,13 @@
+package com.example.testkmp.data
+
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+
+val supabase = createSupabaseClient(
+    supabaseUrl = "https://eistlqkbckeieegjvdrk.supabase.co",
+    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpc3RscWtiY2tlaWVlZ2p2ZHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NzAwMjksImV4cCI6MjA4ODU0NjAyOX0.7PZ3uFSwGokgmq8XCszN8LR8xP1X-HfSuW1Hbega-fU"
+) {
+    install(Postgrest)
+    install(Auth)
+}
