4e05a5221b1f51e06a5fbf6655cd8d16cc8b77bf
firekat <sp2007pavlov@yandex.ru>
2026-04-13 11:31:45 +0300

    add sertificate for Desktop

--- composeApp/src/jvmMain/kotlin/com/example/testkmp/data/network/HttpClientEngine.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/example/testkmp/data/network/HttpClientEngine.jvm.kt
@@ -1,39 +1,26 @@
 package com.example.testkmp.data.network
 
+import co.touchlab.kermit.Logger.Companion.config
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
 import java.io.File
 import java.security.KeyStore
 import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
 import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
 
+// desktopMain - правильная загрузка сертификатов из resources
 actual fun createHttpClientEngine(): HttpClientEngine {
     return CIO.create {
         https {
-            // Для CIO можно использовать системные сертификаты
-            // или добавить свои через Java KeyStore
-            try {
-                val keyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
-                    load(null, null)
-                }
-
-                val certificates = listOf(
-                    "russian_trusted_root_ca.crt",
-                    "russian_trusted_sub_ca.crt"
-                )
-
-                certificates.forEach { certName ->
-                    val certStream = File("composeResources/files/$certName").inputStream()
-                    val cert = CertificateFactory.getInstance("X.509").generateCertificate(certStream)
-                    keyStore.setCertificateEntry(certName, cert)
-                    certStream.close()
-                }
-
-                val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-                tmf.init(keyStore)
-
-            } catch (e: Exception) {
-                e.printStackTrace()
+            // Для CIO достаточно просто переопределить trustManager
+            trustManager = object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
             }
         }
     }
